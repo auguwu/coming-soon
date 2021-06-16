a0c4bb8609337fb7213423c0541b6bcdb710f2c4
Noel <cutie@floofy.dev>
2021-06-16 15:39:01 -0700

    Disable ESLint in `next build`

--- next.config.js
+++ next.config.js
@@ -5,5 +5,9 @@ module.exports = {
   trailingSlash: true,
   images: {
     domains: ['cdn.floofy.dev']
+  },
+  eslint: {
+    // It is already linting in workflow, so this isn't needed
+    ignoreDuringBuilds: true
   }
 };
